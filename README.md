e8d232b741ab68128c183ba0c9bc7b532972957b
Asapanna Rakesh <rakeshark22@gmail.com>
2020-12-04 12:28:36 +0530

    :100:

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # pdf2img
 
-something here again...
+- setup poppler in  windows: https://stackoverflow.com/questions/18381713/how-to-install-poppler-on-windows
+- `pip install poppler-utils`
+
+
+### How to run
+
+activate virtual env and run main.py file
+
+```
+$ .\venv\Scripts\activate
+$ python main.py
+```
--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import os, config
 from pdf2image import convert_from_path
 
 os.makedirs(f"{config.EXTRACTED_IMGS_FOLDER}", exist_ok=True)
-for folder in config.PDFS_FOLDER:
+main_folder = os.listdir(config.PDFS_FOLDER)
+for folder in main_folder:
     
     # save images in folders which are named after every pdf file
     # inside category folders in main folder
     os.makedirs(f"{config.EXTRACTED_IMGS_FOLDER}/{folder}", exist_ok=True)
     
-    for sub_folder in folder:
-        
-        pdfs_path = f"{config.PDFS_FOLDER}/{folder}/{sub_folder}/"
-        pdfs = os.listdir(pdfs_path)
-        os.makedirs(f"{config.EXTRACTED_IMGS_FOLDER}/{folder}/{sub_folder}", exist_ok=True)
-        
-        for filename in pdfs:
-            
-            saved_imgs_path = f"{config.EXTRACTED_IMGS_FOLDER}/{folder}/{sub_folder}/{filename}"
+    for pdffile in os.listdir(f"{config.PDFS_FOLDER}/{folder}"):
+        if pdffile.split(".")[1].lower() == 'pdf':
+            pdf_path = f"{config.PDFS_FOLDER}/{folder}/{pdffile}"
+            print('DEBUG ', pdf_path)
+            # pdf = os.listdir(pdfs_path)
+            os.makedirs(f"{config.EXTRACTED_IMGS_FOLDER}/{folder}/{pdffile}", exist_ok=True)
+                
+            saved_imgs_path = f"{config.EXTRACTED_IMGS_FOLDER}/{folder}/{pdffile}/"
             os.makedirs(saved_imgs_path, exist_ok=True)
-            images = convert_from_path(f'{pdfs_path}{filename}')   
+            filepath = f"{config.PDFS_FOLDER}/{folder}/{pdffile}"
+            print("DEBUG ", filepath)
+            images = convert_from_path(filepath)   
             
             for idx, img in enumerate(images):
-                img.save(f'{saved_imgs_path}/output_{idx}.jpg', 'JPEG')
+                img.save(f'{saved_imgs_path}/page_{idx}.jpg', 'JPEG')
